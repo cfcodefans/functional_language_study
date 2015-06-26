9699684616b3417a43101e118dde7f9e8279d6fa
cfcodefans <cf.chenfan@gmail.com>
2015-06-26 19:00:05 +0800

    .

--- scala-study/src/main/scala/cf/study/lang/BasicTypeAndOperTests.scala
+++ scala-study/src/main/scala/cf/study/lang/BasicTypeAndOperTests.scala
@@ -29,6 +29,16 @@ class BasicTypeAndOperTests {
 			  Type "Help" for help.""")
 		println("""|Welcome to Ultamix 3000.
 			  |Type "Help" for help.""".stripMargin)
+
+		val hello = "hello"
+		println(hello)
+
+		val escape = "\\\"\'"
+		println(escape)
+
+//		val quote = '""'
+		val quote = "''"
+		println(quote)
 	}
 
 	@Test def testSymbol(): Unit = {
@@ -56,5 +66,53 @@ class BasicTypeAndOperTests {
 
 		def pi=3.14
 		println(pi)
+
+		val s = "Hello, world!"
+		println(s, "s indexOf 'o'", s indexOf 'o')
+		println(s, "s indexOf('o')", s indexOf('o'))
+		println(s, "s.indexOf('o')", s.indexOf('o'))
+
+		println(-2.0, "(2.0).unary_- ", (2.0).unary_-)
 	}
+
+	@Test def arithmeticOperations(): Unit = {
+		println("1.2 + 2.3", 1.2 + 2.3)
+		println("3 - 1", 3 - 1)
+		println("'b'-'a'", 'b' - 'a')
+		println("2L * 3L", 2L * 3L)
+		println("11 / 4", 11 / 4)
+		println("11 % 4", 11 % 4)
+		println("11 % -4", 11 % -4)
+		println("-11 % 4", -11 % 4)
+		println("11.0f / 4.0f", 11.0f / 4.0f)
+		println("11.0f % 4.0f", 11.0f % 4.0f)
+
+		println("0.5f % 0.3f", 0.5f % 0.3f)
+
+		val neg = 1 + -3
+		println("neg =", neg, "\t-neg=", -neg)
+		val y = +3
+		println("y =", y)
+	}
+
+	@Test def relationalAndLogicalOperations(): Unit = {
+		println("1 > 2", 1 > 2)
+		println("1 < 2", 1 < 2)
+		println("1.0 <= 1.0", 1.0 <= 1.0)
+		println("1.0 >= 1.0", 1.0 >= 1.0)
+		println("3.5f >= 3.6f", 3.5f >= 3.6f)
+		println("'a' >= 'A'", 'a' >= 'A')
+		println("!true", !true)
+//		println("!true", !!true) //can't compile
+
+		val toBe = true
+		println("toBe", toBe)
+		val question = toBe || !toBe
+		println("toBe || !toBe", question)
+		val paradox = toBe && !toBe
+		println("toBe && !toBe", paradox)
+
+	}
+
+
 }
