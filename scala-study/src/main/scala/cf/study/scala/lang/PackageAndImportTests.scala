ee9c2d6da75d13d60fc8bdab70cd56ed9370352a
cfcodefans <cf.chenfan@gmail.com>
2015-11-18 18:57:29 +0800

    CaseClassTests.scala PackageAndImportTests.scala

--- scala-study/src/main/scala/cf/study/scala/lang/PackageAndImportTests.scala
+++ scala-study/src/main/scala/cf/study/scala/lang/PackageAndImportTests.scala
@@ -150,3 +150,20 @@ package object PackageObject {
 		println(this.getClass)
 	}
 }
+
+class Rocket {
+	import Rocket.fuel
+	private def canGoHomeAgain = fuel > 20
+}
+
+object  Rocket {
+	private def fuel = 10
+	def chooseStrategy(rocket: Rocket): Unit = {
+		if (rocket.canGoHomeAgain)
+			goHome()
+		else
+			pickAStar()
+		def goHome() {}
+		def pickAStar() {}
+	}
+}
