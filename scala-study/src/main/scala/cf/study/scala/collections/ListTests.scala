83588b3af9f7222fbb56008f2d35ee9f1a55f425
cfcodefans <cf.chenfan@gmail.com>
2015-12-29 17:08:14 +0800

    list seems not very useful, as it can't insert element, the ListBuffer seems to work more like java list

--- scala-study/src/main/scala/cf/study/scala/collections/ListTests.scala
+++ scala-study/src/main/scala/cf/study/scala/collections/ListTests.scala
@@ -1,6 +1,11 @@
 package cf.study.scala.collections
 
+import java.util.Date
+
+import org.apache.commons.lang3.builder.ToStringBuilder
 import org.junit.{Assert, Test}
+
+import scala.collection.mutable.ListBuffer
 
 /**
  * Created by fan on 2015/9/14.
@@ -36,11 +41,148 @@ class ListTests {
 		println(fruits.tail)
 	}
 
-	@Test def testIsEmpty: Unit = {
+	@Test def testLast: Unit = {
+		val fruits: List[String] = List("apples", "oranges", "pears", "bananas")
+		println(fruits.last)
+	}
 
+	@Test def testInit: Unit = {
+		val fruits: List[String] = List("apples", "oranges", "pears", "bananas")
+		println(fruits.init)
+	}
+
+	@Test def testIsEmpty: Unit = {
+		val list = List()
+		Assert.assertTrue(list.isEmpty)
+		Assert.assertFalse(list + "" isEmpty)
+	}
+
+	@Test def testReverse(): Unit = {
+		println(List(1, 2, 3, 4, 5).reverse)
 	}
 
 	@Test def contructLists(): Unit = {
-		val fruit = "apples" :: ("oranges" :: ("pears" :: Nil))
+		val fruits = "apples" :: ("oranges" :: ("pears" :: Nil))
+		println(fruits.size)
+		println(ToStringBuilder.reflectionToString(fruits))
+
+		val nums = 1 :: (2 :: (3 :: (4 :: Nil)))
+		Assert.assertEquals(nums.head, 1)
+		Assert.assertEquals(nums.last, 4)
+	}
+
+	@Test def testInsert(): Unit = {
+		val nums:ListBuffer[Int] = ListBuffer(1, 2, 3, 4)
+		Assert.assertEquals(2, nums(1))
+		nums.insert(1, 1)
+		Assert.assertEquals(1, nums(1))
+		Assert.assertEquals(2, nums(2))
+	}
+
+	@Test def testInsertSort(): Unit = {
+		def insert(x: Int, xs: List[Int]): List[Int] =
+			if (xs.isEmpty || x <= xs.head) x :: xs
+			else xs.head :: insert(x, xs.tail)
+
+		def isort(xs: List[Int]): List[Int] =
+			if (xs.isEmpty) xs
+			else insert(xs.head, isort(xs.tail))
+
+		val nums:List[Int] = List(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7)
+		println(isort(nums))
+	}
+
+	@Test def testListPatterns(): Unit = {
+		val fruits: List[String] = List("Apple", "Banana", "Cherry")
+		val List(a, b, c) = fruits
+		Assert.assertEquals("Apple", a)
+		Assert.assertEquals("Banana", b)
+		Assert.assertEquals("Cherry", c)
+
+//		val List(head, rest) = fruits //match error
+		val head :: rest = fruits
+		Assert.assertEquals("Apple", head)
+		Assert.assertEquals(fruits.tail, rest)
+	}
+
+	@Test def testFirstOrderListMethods(): Unit = {
+		Assert.assertEquals(List(1, 2) ::: List(3, 4, 5), List(1, 2, 3, 4, 5))
+		Assert.assertEquals(List() ::: List(1, 2, 3), List(1, 2, 3))
+		Assert.assertEquals(List(1, 2, 3) ::: List(4), List(1, 2, 3, 4))
+	}
+
+	@Test def testPrefixAndSuffix(): Unit = {
+		val abcde = List('a', 'b', 'c', 'd', 'e')
+		val List(a, b, c, d, e) = abcde
+		Assert.assertEquals(abcde.take(2), List(a, b))
+		Assert.assertEquals(abcde.takeRight(2), List(d, e))
+		Assert.assertEquals(abcde.drop(2), List(c, d, e))
+		Assert.assertEquals(abcde.dropRight(2), List(a, b, c))
+		Assert.assertEquals(abcde.splitAt(2), (List(a, b), List(c, d, e)))
+		Assert.assertEquals(abcde.apply(2), c)
+	}
+
+	@Test def testFlatten(): Unit = {
+		println(List(List(1, 2), List('a'), List(), List(new Date())).flatten)
+	}
+
+	@Test def testIndices(): Unit = {
+		val abcde = List('a', 'b', 'c', 'd', 'e')
+		println(abcde.indices)
+	}
+
+	@Test def testZip(): Unit = {
+		val abcde = List('a', 'b', 'c', 'd', 'e')
+		println(abcde.indices.toList.zip(abcde))
+
+		println(abcde.zip(List(1, 2, 3)))
+
+		println(abcde.zipWithIndex)
+
+		println(abcde.zipWithIndex.unzip)
+	}
+
+	@Test def testWithString(): Unit = {
+		val abcde = List('a', 'b', 'c', 'd', 'e')
+		println(abcde.mkString("\t"))
+		println(abcde.mkString("[", ", ", "]"))
+	}
+
+	@Test def testConvertingList(): Unit = {
+		val abcde = List('a', 'b', 'c', 'd', 'e')
+		println(abcde)
+		val array = abcde.toArray
+		println(array.mkString(", "))
+		array(1) = 'p'
+		println(array.mkString(", "))
+		println(abcde)
+
+		val it = abcde.iterator
+		println(it.next)
+		println(it.next)
+	}
+
+	@Test def testMergeSort() {
+		def msort[T](less: (T, T) => Boolean) (xs: List[T]) : List[T] = {
+			def merge(xs: List[T], ys: List[T]): List[T] =
+				(xs, ys) match {
+					case (Nil, _) => ys
+					case (_, Nil) => xs
+					case (x :: xs1, y :: ys1) =>
+						if (less(x, y)) x :: merge(xs1, ys)
+						else y :: merge(xs, ys1)
+				}
+
+			val n = xs.length / 2
+			if (n == 0) xs
+			else {
+				val (ys, zs) = xs.splitAt(n)
+				merge(msort(less)(ys), msort(less)(zs))
+			}
+		}
+
+		val nums:List[Int] = List(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7)
+		println(msort((_1:Int, _2:Int) => _1 < _2)(nums))
+		println(msort((_1:Int, _2:Int) => _1 > _2)(nums))
 	}
 }
