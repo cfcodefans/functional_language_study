687e060a49fd8bcfef1e47e47a07e3e4536eb10e
fan <cf.chenfan@gmail.com>
2016-08-19 09:31:27 +0800

    ....

--- data-study/systemjs.config.js
+++ data-study/systemjs.config.js
@@ -8,10 +8,14 @@
   "router",
   "platform-browser",
   "platform-browser-dynamic",
+
+  // "@angular2-material/core",
+  // "@angular2-material/sidenav",
+
     // "upgrade"
     ];
 
-    const NG_VERSION = "2.0.0-rc.4";
+    const NG_VERSION = "2.0.0-rc.5";
     const RESPOSITORY = "//npmcdn.com";
 
     function MAP() {
@@ -22,6 +26,9 @@
 
       NG_PACKAGES.forEach((_package) => map[`@angular/${_package}`] = `${RESPOSITORY}/@angular/${_package}@${NG_VERSION}`);
       map["@angular/router"] = `${RESPOSITORY}/@angular/router@3.0.0-alpha.7`;
+
+      // map["@angular2-material/core"] = `${RESPOSITORY}/@angular2-material/core@2.0.0-alpha.7-3`;
+      // map["@angular2-material/sidenav"] = `${RESPOSITORY}/@angular2-material/sidenav@2.0.0-alpha.7-3`;
       return map;
     }
 
