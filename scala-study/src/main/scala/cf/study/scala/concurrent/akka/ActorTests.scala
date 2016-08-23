44927133f75c5b48d6bda7fa00eb0b089b5315e5
cfcodefans <cf.chenfan@gmail.com>
2016-08-23 19:20:52 +0800

    ..

--- scala-study/src/main/scala/cf/study/scala/concurrent/akka/ActorTests.scala
+++ scala-study/src/main/scala/cf/study/scala/concurrent/akka/ActorTests.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.{Callable, TimeUnit}
 
 import akka.actor.{Actor, ActorSystem, PoisonPill, Props}
 import akka.dispatch.Futures
+import akka.routing.RoundRobinPool
 import org.junit.Test
 
 import scala.concurrent.duration.Duration
@@ -78,4 +79,66 @@ class ActorTests {
 		//		}) (ExecutionContext.global)
 	}
 
+	@Test def sumExample = {
+		sealed trait SumTrait
+		case class Result(value: Int) extends SumTrait
+
+		class SumActor extends Actor {
+			val RANGE = 10000
+
+			def calculate(start: Int, end: Int, flag: String): Int = {
+				var cal = 0
+				for (i <- (start to end)) {
+					for (j <- 1 to 3000000) {}
+					cal += 1
+				}
+				println(s"flag : ${flag}.")
+				return cal
+			}
+
+			override def receive: Receive = {
+				case value: Int =>
+					sender() ! Result(calculate((RANGE / 4) * (value - 1) + 1, (RANGE / 4) * value, value.toString))
+				case _ => println("unknown in SumActor...")
+			}
+		}
+
+		class PrintActor extends Actor {
+			override def receive: Receive = {
+				case (sum: Int, startTime: Long) =>
+					println(s"Sum: ${sum}; time spent: ${(System.nanoTime() - startTime) / 1000000000.0} seconds")
+				case _ => println("unknown in PrintActor...")
+			}
+		}
+
+		class MasterActor extends Actor {
+			var sum = 0
+			var count = 0
+			var startTime: Long = 0
+
+			val sumActor = context.actorOf(Props[SumActor](new SumActor).withRouter(RoundRobinPool(nrOfInstances = 4)), name = "sumActor")
+			val printActor = context.actorOf(Props[PrintActor](new PrintActor), name = "printActor")
+
+			override def receive: Receive = {
+				case "calculate..." =>
+					startTime = System.nanoTime
+					for (i <- 1 to 4) sumActor ! i
+				case Result(value) =>
+					sum += value
+					count += 1
+					if (count == 4) {
+						printActor ! (sum, startTime)
+						context.stop(self)
+					}
+				case _ => println(s"unknown in MasterActor...")
+			}
+		}
+
+		var sum = 0
+		val system = ActorSystem("MasterActorSystem")
+		val masterActor = system.actorOf(Props[MasterActor](new MasterActor), name = "masterActor")
+		masterActor ! "calculate..."
+
+		Thread.sleep(5000)
+	}
 }
--- sqoop-scala/src/main/scala/org/apache/sqoop/model/MConnector.scala
+++ sqoop-scala/src/main/scala/org/apache/sqoop/model/MConnector.scala
@@ -1,6 +1,7 @@
 package org.apache.sqoop.model
 
 import org.apache.sqoop.classification.{InterfaceAudience, InterfaceStability}
+import org.apache.sqoop.model.MConfigurableType.MConfigurableType
 
 /**
   * Connector entity supports the FROM/TO {@link org.apache.sqoop.job.etl.Transfereable} Includes unique id
@@ -9,6 +10,78 @@ import org.apache.sqoop.classification.{InterfaceAudience, InterfaceStability}
   */
 @InterfaceAudience.Public
 @InterfaceStability.Unstable
-class MConnector extends Configurable {
+class MConnector(val uniqueName: String,
+                 val className: String,
+                 val version: String,
+                 val linkConfig: MLinkConfig,
+                 val fromConfig: MFromConfig,
+                 val toConfig: MToConfig
+                ) extends Configurable {
 	//TODO
+	if (uniqueName == null || className == null) throw new NullPointerException
+
+	def getUniqueName: String = uniqueName
+
+	def getClassName: String = className
+
+	def getLinkConfig: MLinkConfig = linkConfig
+
+	def getFromConfig: MFromConfig = fromConfig
+
+	def getToConfig: MToConfig = toConfig
+
+	def getVersion: String = version
+
+	def getType: MConfigurableType = MConfigurableType.CONNECTOR
+
+	def clone(cloneWithValue: Boolean): MConnector = {
+		// Connector never have any values filled
+		val isCloneWithValue: Boolean = false
+		var fromConfig: MFromConfig = this.getFromConfig
+		var toConfig: MToConfig = this.getToConfig
+		if (fromConfig != null) fromConfig = fromConfig.clone(isCloneWithValue)
+		if (toConfig != null) toConfig = toConfig.clone(isCloneWithValue)
+		val copy: MConnector = new MConnector(this.getUniqueName, this.getClassName, this.getVersion, this.getLinkConfig.clone(isCloneWithValue), fromConfig, toConfig)
+		copy.setPersistenceId(this.getPersistenceId)
+		copy
+	}
+
+	def getSupportedDirections: SupportedDirections = new SupportedDirections(this.getFromConfig != null, this.getToConfig != null)
+
+	override def hashCode: Int = {
+		val supportedDirections: SupportedDirections = getSupportedDirections
+		var result: Int = getLinkConfig.hashCode
+		if (supportedDirections.isDirectionSupported(Direction.FROM)) result = 31 * result + getFromConfig.hashCode
+		if (supportedDirections.isDirectionSupported(Direction.TO)) result = 31 * result + getToConfig.hashCode
+		result = 31 * result + version.hashCode
+		result = 31 * result + uniqueName.hashCode
+		result = 31 * result + className.hashCode
+		result
+	}
+
+	override def equals(other: Any): Boolean = {
+		if (other.asInstanceOf[AnyRef] eq this) return true
+		if (!other.isInstanceOf[MConnector]) return false
+		val mc: MConnector = other.asInstanceOf[MConnector]
+		val supportedDirections: SupportedDirections = this.getSupportedDirections
+		val mcSupportedDirections: SupportedDirections = mc.getSupportedDirections
+
+		if (supportedDirections.isDirectionSupported(Direction.FROM)
+			&& mcSupportedDirections.isDirectionSupported(Direction.FROM)
+			&& getFromConfig != mc.getFromConfig)
+			return false
+
+		if (supportedDirections.isDirectionSupported(Direction.FROM) != mcSupportedDirections.isDirectionSupported(Direction.FROM))
+			return false
+
+		if (supportedDirections.isDirectionSupported(Direction.TO)
+			&& mcSupportedDirections.isDirectionSupported(Direction.TO)
+			&& getToConfig != mc.getToConfig)
+			return false
+
+		if (supportedDirections.isDirectionSupported(Direction.TO) != mcSupportedDirections.isDirectionSupported(Direction.TO))
+			return false
+
+		uniqueName == mc.uniqueName && className == mc.className && version == mc.version && linkConfig == mc.getLinkConfig
+	}
 }
