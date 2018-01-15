807b6e99f1eea113dcfdb0e42e38c7cf7912c7d9
cfcodefans <cf.chenfan@gmail.com>
2018-01-15 22:59:30 +0800

    ..

--- crypto-currency/app/binance-api.ts
+++ crypto-currency/app/binance-api.ts
@@ -1,6 +1,7 @@
 import * as Req from 'request'
 import * as RP from 'request-promise'
 import * as _ from 'lodash'
+import * as crypto from 'crypto'
 
 export const BASE: string = "https://api.binance.com/api/"
 export const WS: string = 'https://api.binance.com/wapi/'
@@ -20,6 +21,8 @@ export function opt(): Options {
     return new Options
 }
 
+export const OPTS: Options = new Options()
+
 export async function publicReq(_url: string, data: any, _method: string = 'GET'): Promise<any> {
     if (!(data && !_.isEmpty(_url))) return Promise.resolve(null);
 
@@ -27,11 +30,45 @@ export async function publicReq(_url: string, data: any, _method: string = 'GET'
         url: _url,
         method: _method,
         qs: data,
-        timeout: opt().recvWindow,
+        timeout: OPTS.recvWindow,
         headers: {
             'User-agent': USER_AGENT,
             'Content-type': CONTENT_TYPE
         }
-    })
+    }).then((resp: Req.RequestResponse) => resp.body)
+        .then(JSON.parse)
+        .catch(console.error)
+}
+
+export async function apiReq(_url: string, _method: string = 'GET'): Promise<any> {
+    if (_.isEmpty(OPTS.apiKey)) throw 'apiReq: Invalid API Key';
+    return RP({
+        url: _url,
+        method: _method,
+        timeout: OPTS.recvWindow,
+        // agent:false,
+        headers: {
+            'User-agent': USER_AGENT,
+            'Content-type': CONTENT_TYPE,
+            'X-MBX-APIKEY': OPTS.apiKey
+        }
+    }).then((resp: Req.RequestResponse) => resp.body)
+        .then(JSON.parse)
+        .catch(console.error)
+}
+
+export async function signedReq(_url: string, data: any, _method: string = 'GET'): Promise<any> {
+    if (_.isEmpty(OPTS.apiSecret)) throw 'signedReq: Invalid API Secret'
+    data = data ? data : {}
+    data.timestamp = new Date().getTime()
+    if (typeof data.symbol !== 'undefined') data.symbol = data.symbol.replace('_', '')
+    if (typeof data.recvWindow === 'undefined') data.recvWindow = OPTS.recvWindow
+
+    let query: string = Object.keys(data)
+        .map((key: string) => `${key}=${encodeURIComponent(data[key])}`)
+        .join('&')
+
+    let signature: string = crypto.createHmac('sha256', OPTS.apiSecret).update(query).digest('hex')
+    return apiReq(`${_url}?${query}&signature=${signature}`)
 }
 
--- crypto-currency/app/main.ts
+++ crypto-currency/app/main.ts
@@ -1,5 +1,6 @@
 import * as bn from "./binance-api"
 import * as RP from 'request-promise'
+import * as _ from "lodash"
 
 async function main() {
     console.info("test")
@@ -7,6 +8,10 @@ async function main() {
     // console.info(bn.BASE)
     let req = await RP("http://baidu.com")
     console.info(req)
+
+    let d = {}
+    _.update(d, "symbol", (symbol: string) => symbol)
+    console.info(d)
 }
 
 main()
